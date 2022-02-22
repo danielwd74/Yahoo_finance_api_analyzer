e137a2e07eead4ab6f1eb770533800c07810955b
Daniel Doherty <danielwd74@gmail.com>
2022-02-22 12:34:47 -0500

    First commit

---
+++ config2.py
@@ -0,0 +1,27 @@
+import requests as rq
+import pandas as pd
+import json
+import datetime
+
+getTicker = input("Please enter a valid ticker: ")
+ticker = str(getTicker).upper()
+
+url = f'https://yfapi.net/v7/finance/options/{ticker}'
+personal_api_key = {'x-api-key':'8MM0rOwJ2T841kw8xbn1m7rYBRIy7aH11SN2gX6G'}
+
+response = rq.request("GET", url, headers=personal_api_key)
+responseJSON = json.loads(response.text)
+
+#print((responseJSON['optionChain']['result'][0]['options'][0]['calls'][2]))
+#responseCallOptions = json.loads(responseJSON['optionChain']['result'][0]['options'][0]['calls'])
+callOptions = responseJSON['optionChain']['result'][0]['options'][0]['calls']
+#print(responseResult)
+responseSTR = responseJSON
+
+row_labels = ['contractSymbol']
+df = pd.DataFrame(data=callOptions)
+
+dt = str(datetime.datetime.now())
+dt = dt[:dt.find(' ')]
+#ts = datetime.timestamp(dt)
+df.to_csv(f'{dt}_{ticker}.csv', sep=",")
